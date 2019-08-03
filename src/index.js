92cf0c3f95c92f8c38f0478c8ed9c928db3f54da
Brian Sunbury <bsunbury29@gmail.com>
2019-08-02 19:32:09 -0700

    initial commit of clone

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './bootstrap.css';
+// import './creative.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
