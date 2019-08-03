5d0619bf9fb838a501e3b5156a8553380a216903
Brian Sunbury <bsunbury29@gmail.com>
2019-08-03 00:20:12 -0700

    Updates

---
+++ precache-manifest.bcce7c3c2e407f19eafb560c7521a62c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d068a88a50a8e71ef385f31eee33a1ec",
+    "url": "/StanleyBulldogs2/index.html"
+  },
+  {
+    "revision": "917baa027c1993b63d96",
+    "url": "/StanleyBulldogs2/static/js/2.16989715.chunk.js"
+  },
+  {
+    "revision": "6b5a3dd1a2a7441abff4",
+    "url": "/StanleyBulldogs2/static/js/main.20080cf7.chunk.js"
+  },
+  {
+    "revision": "cd6c99d4c0ac7ca2cf8f",
+    "url": "/StanleyBulldogs2/static/js/runtime~main.b60b4557.js"
+  },
+  {
+    "revision": "1d11222c20a810c4759b8c8591456592",
+    "url": "/StanleyBulldogs2/static/media/01.1d11222c.jpg"
+  },
+  {
+    "revision": "75907b6c1f9d7c96e5c1b2f64ea1b41e",
+    "url": "/StanleyBulldogs2/static/media/02.75907b6c.jpg"
+  },
+  {
+    "revision": "a97374e94ee5914e92ecde754faafc7a",
+    "url": "/StanleyBulldogs2/static/media/03.a97374e9.jpg"
+  },
+  {
+    "revision": "89496bc2fd72d0f9432370d50c6292c4",
+    "url": "/StanleyBulldogs2/static/media/04.89496bc2.jpg"
+  },
+  {
+    "revision": "2c172bdcb68b96098adcb166ae2f23f7",
+    "url": "/StanleyBulldogs2/static/media/05.2c172bdc.jpg"
+  },
+  {
+    "revision": "98f163b5999d7c9fb1c042bb2abc6735",
+    "url": "/StanleyBulldogs2/static/media/gusnrosie.98f163b5.jpg"
+  },
+  {
+    "revision": "39cde34e3b3937715753a87b3f2017da",
+    "url": "/StanleyBulldogs2/static/media/hannah.39cde34e.jpg"
+  },
+  {
+    "revision": "4324220262acf6efa97195e1f7d6e3be",
+    "url": "/StanleyBulldogs2/static/media/maggie.43242202.jpg"
+  },
+  {
+    "revision": "e2aae9b53d9e1b72e38ddf4c40e232a4",
+    "url": "/StanleyBulldogs2/static/media/stan-logo.e2aae9b5.jpg"
+  },
+  {
+    "revision": "7ad97443106a9d246ee7d4f45e8e9159",
+    "url": "/StanleyBulldogs2/static/media/stanley.7ad97443.jpg"
+  }
+]);
