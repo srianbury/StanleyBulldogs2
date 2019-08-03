eacaaa0425363db8cff424a74b78c00c153f9e2a
Brian Sunbury <bsunbury29@gmail.com>
2019-08-03 00:54:45 -0700

    domain name config

--- src/Components/Announcements/index.js
+++ src/Components/Announcements/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 
 const Announcements = () => (
-	<div className='mt-4'>
+	<div className='mt-4 mx-2'>
 		<div className='alert alert-info'>
-			<h2 className="section-heading text-center">Announcements!</h2>
 			<p className='text-center'>
-				Hannah and Stanley just had puppies!! (July 2019).  Visit our <a href="https://www.instagram.com/stanleybulldogs/" target="_blank" rel="noopener noreferrer">instagram</a> for more info while we get their
+				<h2>Hannah and Stanley just had 8 puppies!!</h2> (July 2019).  Visit our <a href="https://www.instagram.com/stanleybulldogs/" target="_blank" rel="noopener noreferrer">instagram</a> for more info while we get their
 				profile pics ready!
 			</p>
 		</div>
--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import logo from '../../images/stan-logo.jpg';
 
 const Nav = () => (
 	<nav className="navbar navbar-expand-lg navbar-light fixed-top navbar-shrink" id="mainNav">
 		<div className="container">
-			<a className="navbar-brand" href="#page-top">Stanley Bulldogs</a>
+			<a className="navbar-brand" href="#page-top">
+				Stanley Bulldogs
+			</a>
 			<button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
 				<span className="navbar-toggler-icon"></span>
 			</button>
