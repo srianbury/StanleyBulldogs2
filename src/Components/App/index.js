0ea7c742f7f72b8509d996e52f45b3b3a7354e46
Brian Sunbury <bsunbury29@gmail.com>
2020-03-29 13:37:53 -0700

    fix: current litter if exists

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import { db } from '../../Constants';
-import Announcements from '../Announcements';
-import Nav from '../Nav';
-import ImageHeader from '../ImageHeader';
-import Parents from '../Parents';
-import Litters, { CurrentLitter } from '../Litters';
-import Contact from '../Contact';
-
+import React from "react";
+import { db } from "../../Constants";
+import Announcements from "../Announcements";
+import Nav from "../Nav";
+import ImageHeader from "../ImageHeader";
+import Parents from "../Parents";
+import Litters, { CurrentLitter } from "../Litters";
+import Contact from "../Contact";
 
 const App = () => (
-	<>
-		<Nav />
-		<ImageHeader />
-		{false && <Announcements />}
-		<CurrentLitter 
-			litter={db.currentLitter} />
-		<Litters 
-            litters={db.litters} />
-		<Parents 
-            parents={db.parents} />
-		<Contact />
-	</>
+  <>
+    <Nav />
+    <ImageHeader />
+    {false && <Announcements />}
+    {db.currentLitter && <CurrentLitter litter={db.currentLitter} />}
+    <Litters litters={db.litters} />
+    <Parents parents={db.parents} />
+    <Contact />
+  </>
 );
 
 export default App;
